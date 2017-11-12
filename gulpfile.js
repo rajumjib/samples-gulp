d569c4cea2717711ef2c6395ddb6da9a33f1305f
Jahirul Islam Bhuiyan <0nline4help@gmail.com>
2017-11-12 13:16:02 +0600

    updated

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,11 @@
 var gulp = require('gulp');
 var translateLanguage = require('gulp-translate-properties');
+var convertToUnicode = require('gulp-translate-properties/convert');
 var postfix = require('gulp-translate-properties/rename');
 gulp.task('default', function() {
   return gulp.src(['lang/*.properties','lang/*/*.properties'])
-    .pipe(translateLanguage({"APIKey":'AIzaSyA860dmW6T0VZEbO7R1mgJNMld233mNDMk',"translateTo":'bn'}))
+    .pipe(translateLanguage({"APIKey":'',"translateTo":'bn'}))
+    .pipe(convertToUnicode())
 	.pipe(postfix('_bn_BD'))
     .pipe(gulp.dest('dist'));
 });
